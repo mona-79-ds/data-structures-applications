904d745b654831c2c2e29c86a244ab57714bfc83
mona-79-ds <monikavt79@gmail.com>
2025-11-30 15:46:10 +0530

    Create p8_BST_p_i_p_recursive.c

---
+++ p8_BST_p_i_p_recursive.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * left;
+    struct node * right;
+};
+typedef struct node * NODE;
+
+NODE createBST(NODE root, int key)
+{
+    NODE temp,cur,prev;
+    temp=(NODE)malloc(sizeof(struct node));
+    temp->data= key;
+    temp->left=NULL;
+    temp->right=NULL;
+    if(root==NULL)
+        return temp;
+    cur=root;
+    prev=NULL;
+
+    while(cur!=NULL)
+    {
+        prev=cur;
+        if(key < cur->data)
+            cur=cur->left;
+        else
+            cur=cur->right;
+    }
+    if(key < prev->data)
+        prev->left = temp;
+    else
+        prev->right = temp;
+    return root;
+}
+
+void preorder(NODE root)
+{
+    if(root!=NULL)
+    {
+        printf("\%d\t",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void inorder(NODE root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->left);
+        printf("%d\t",root->data);
+        inorder(root->right);
+    }
+}
+
+void postorder(NODE root)
+{
+    if(root!=NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d\t",root->data);
+    }
+}
+
+int main()
+{
+    NODE root = NULL;
+    int item,ch;
+    for(;;)
+    {
+        printf("\n1.INSERT");
+        printf("\n2.PREORDER");
+        printf("\n3.INORDER");
+        printf("\n4.POSTORDER");
+        printf("\n5.EXIT");
+        printf("\n Read choice: ");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+        case 1:printf("\nRead item to be inserted:");
+               scanf("%d",&item);
+               root=createBST(root,item);
+               break;
+        case 2: printf("\n The Preorder transversal is \n");
+                preorder(root);
+                break;
+        case 3:printf("\n The Inorder transversal");
+               inorder(root);
+               break;
+        case 4:printf("\n The Postorder transversal");
+               postorder(root);
+               break;
+        default:exit(0);
+        }
+    }
+    return 0;
+}
